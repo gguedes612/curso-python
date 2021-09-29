1cf9edfea2d27ac3f139968fb38c8f9eb5e549c0
gguedes612 <gguedes612@gmail.com>
2021-09-29 06:00:55 -0300

    Atividades Seçao - 4

---
+++ recebendo-dados-usuario.py
@@ -0,0 +1,14 @@
+""" 
+    Input -> Entrada de dados
+    !!! TODO DADO RECEBIDO VIA INPUT É DO TIPO STRING!!!
+"""
+
+nome = input("Digite seu nome: ") 
+
+idade = input("Digite sua idade: ")
+
+# Print -> Saída de dados
+
+print(f"Seja bem-vindo(a) {nome}") 
+
+print(f"{nome} tem {idade} anos. ")
