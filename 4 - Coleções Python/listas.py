a3dfb2a4ed984b253e2e06b3e96236ac26c98c84
gguedes612 <gguedes612@gmail.com>
2021-11-11 22:23:59 -0300

    Adicionando anotaçoes de listas

---
+++ 4 - Coleções Python/listas.py
@@ -0,0 +1,60 @@
+#Printando lista
+lista = [2, 1, 5, 3, 8, 3, 8, 4, 3]
+
+#Percorrendo lista
+for i in range(len(lista)) :
+    print(lista[i])
+
+#Condicional com lista
+if 2 in lista:
+    print("Existe o elemento 2 na lista")
+else:
+    print("Não existe o elemento 2 na lista")
+
+#Ordenar lista
+lista.sort()
+
+#Contar elemento na lista
+print(lista.count(3))
+
+#Adicionar um unico elemento na lista
+lista.append(1)
+
+#Adicionando lista dentro de outra lista, como elemento unico
+lista.extend([1,2,3,4])
+
+#Inserir valor no index
+lista.insert(2,"Novo valor")
+
+#Ordenar a lista reversa
+lista.reverse()
+print(lista[::-1])
+
+#Copiar uma lista
+listanova = lista.copy()
+
+#Contar elementos de uma lista
+print(len(lista))
+
+#Remover ultimo elemento e retorna o ultimo elemento, pode passar um index como parametro
+lista.pop()
+
+#Remover todos os elementos de uma lista
+lista.clear()
+
+#Separando em lista string
+frase = "meu nome"
+frase = frase.split()
+print(frase)
+
+
+
+
+
+
+
+
+
+
+
+print(lista)
